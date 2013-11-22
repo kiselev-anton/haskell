09d6f6cb84c780908a6754836f20dacfd3793de8
kiselev <kiselev@planet-a.ru>
2013-11-22 23:23:15 +0600

    added #3,#4 tasks

--- sixth.hs
+++ sixth.hs
@@ -3,6 +3,19 @@ import Control.Applicative
 import Control.Monad.Writer
 import Data.List
 
+{-
+##################################################
+
+Status:
+#1: done
+#2: done
+#3: ready
+#4: ready
+#5: done, but should be reworked
+#6: not started
+
+-}
+
 -- #1 ready
 data (Num t, Show t, Eq t) => TLine t = Line {a::t, b::t, c::t}
     deriving(Show, Eq)
@@ -58,11 +71,32 @@ cartesianProduct5 list1 list2 = do
     b <- list2
     return [a,b]
 
--- #4 bugged
+-- #4 ready
 -- a=1, b=2, c,d=10, e=1,11,21
 -- String = [Char]
 
--- TODO: fix repetitions
+-- Использование бинарных деревьев даёт выйгрыш в паре миллисекунд по сравнению с nub
+data (Eq a, Ord a) => Tree a = Empty | Node {element::a, left::Tree a, right::Tree a}
+        deriving(Eq, Show)
+
+empty' :: Tree a -> Bool
+empty' Empty = True
+empty' _ = False
+
+add :: (Eq a, Ord a) => a -> Tree a -> Tree a
+add value tree
+        | empty' tree = (Node value Empty Empty)
+        | value == element tree = tree
+        | value > element tree = (Node (element tree) (left tree) (add value (right tree)))
+        | otherwise = (Node (element tree) (add value (left tree)) (right tree))
+
+treeToList :: (Ord a) => Tree a -> [a]
+treeToList Empty = []
+treeToList (Node element left right) = treeToList left ++ [element] ++ treeToList right
+
+listToTree :: (Ord a) => [a] -> Tree a
+listToTree [] = Empty
+listToTree (x:xs) = add x $ listToTree xs
 
 charValues :: Char -> [Int]
 charValues x
@@ -73,12 +107,12 @@ charValues x
     | otherwise = []
 stringValues :: String -> [Int]
 stringValues [x] = charValues x
-stringValues string = 
-    foldl1 (\list1 list2 -> map sum $ cartesianProduct1 list1 list2) $ map charValues string
+stringValues (x:xs) = [elements | elements <- treeToList $ listToTree [a+b |a <- charValues x, b <- stringValues xs]]
 
 
 
--- #5 ready
+-- #5 ready, but code is terrible
+-- TODO: complete rework
 --maxWithHistory :: 
 maxWithHistory list = 
     let maxHistory = maxWithHistoryTemp list
@@ -103,3 +137,8 @@ maxWithHistoryTemp (x:xs) = runWriter $ foldl
 -- 3) Все последовательные действия склеиваем и убираем лишнее с конца.
 
 -- P.S. Код не очень хороший и суть монады writer не используется, используется лишь та её часть, которая похожа на пару.
+
+-- #6
+
+
+
